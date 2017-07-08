08c9b57557667f9af28953a31a0c2370b33d8e63
Denis Postika <den.postika@gmail.com>
2017-07-09 01:19:11 +0300

    Alpha

---
+++ commands/add.js
@@ -0,0 +1,19 @@
+var request = require('request'),
+    util = require('../util'),
+    app = require('../index'),
+    deezer = require('deezer-node-api');
+
+var dz = new deezer();
+
+module.exports = function (param) {
+    var channel = param.channel;
+
+    dz.findTracks(param.args.join(' ')).then(function (result) {
+        if (result.data[0]) {
+            app.io().sockets.emit('add', result.data[0].id);
+            util.postMessage(channel, "Заявку принял на " + result.data[0].artist.name +" - "+ result.data[0].title);
+        } else {
+            util.postMessage(channel, "Мне не удалось ничего найти чувак :с");
+        }
+    });
+};
---
+++ index.js
@@ -0,0 +1,40 @@
+var slackTerminal = require('slack-terminalize');
+
+var express = require('express');
+var app = express();
+
+var server = app.listen(8080);
+var io = require('socket.io').listen(server);
+var gm = require('locutus/php/datetime/date');
+
+slackTerminal.init('xoxb-210718922887-VYKp2JOgWzQDRGNF9kBdtoz9', {
+    // slack rtm client options here
+    // more info at: https://github.com/slackhq/node-slack-client/blob/master/lib/clients/rtm/client.js
+}, {
+    // app configurations to suit your project structure
+    // to see the list of all possible config,
+    // check this out: https://github.com/ggauravr/slack-terminalize/blob/master/util/config.js
+	CONFIG_DIR: __dirname + '/config',
+	COMMAND_DIR: __dirname + '/commands'
+});
+
+app.get('/', function(req, res, next) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/channel', function(req, res, next) {
+    var cache_expire = 60*60*24*365;
+    var date = new Date();
+
+    res.set({
+        'Pragma': 'public',
+        'Cache-Control': 'maxage=' + cache_expire,
+        'Expires': gm.date('D, d M Y H:i:s', date.getTime() + cache_expire) + ' GMT'
+    })
+
+    res.sendFile(__dirname + '/chanel.html');
+});
+
+exports.io = function () {
+    return io;
+};
