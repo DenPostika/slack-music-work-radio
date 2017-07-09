c9c3c681001a99108194e2d9a6ce662c7c079835
Denis Postika <den.postika@gmail.com>
2017-07-09 03:12:41 +0300

    Add clients count condition

--- commands/add.js
+++ commands/add.js
@@ -10,11 +10,11 @@ module.exports = function (param) {
 
     dz.findTracks(param.args.join(' ')).then(function (result) {
         if (result.data[0]) {
-            if (app.io().sockets.clients()) {
+            if (app.io().engine.clientsCount) {
                 app.io().sockets.emit('add', result.data[0].id);
                 util.postMessage(channel, "Заявку принял на " + result.data[0].artist.name + " - " + result.data[0].title);
             } else {
-                util.postMessage(channel, "Прости заявку то я принял, но играть негде :с");
+                util.postMessage(channel, "Прости, заявку то я принял, но играть негде :с");
             }
         } else {
             util.postMessage(channel, "Мне не удалось ничего найти чувак :с");
